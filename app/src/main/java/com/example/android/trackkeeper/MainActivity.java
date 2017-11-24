9ce8750d5f1fa1ffd13795cfeec2c8162b6e10b6
Jesper Bjerregaard <jebj87@gmail.com>
2017-11-24 23:13:26 +0100

    Initial commit

---
+++ app/src/main/java/com/example/android/trackkeeper/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.android.trackkeeper;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // remove title
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+    }
+}
